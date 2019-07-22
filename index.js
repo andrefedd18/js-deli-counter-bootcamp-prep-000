96bc5c2fd800d8498283c916df756de3fbd5ad87
alfedd Fedd <alfedd@crimson.ua.edu>
2019-07-22 05:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function takeANumber(katzDeli, Ada) {
 function nowServing(katzDeliLine, nowServing) {
   function nowServing(katzDeli) {
     let i = 0 ;
-    
+    while (i < katzDeli.length) {
+    i++;
   }
-}
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+  }
