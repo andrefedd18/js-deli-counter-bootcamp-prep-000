0af35436bf2a75bdab7b2596fedbd0c308c19838
alfedd Fedd <alfedd@crimson.ua.edu>
2019-07-22 05:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeli) {
 function currentLine(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
-     line.push(` `+[i+1]+`. `  + katzDeli[i])
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
     i++;
   }
   if (katzDeli.length === 0) {
