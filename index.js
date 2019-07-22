b3250087369abf1f50cbd8bda55f295efabccdaa
alfedd Fedd <alfedd@crimson.ua.edu>
2019-07-22 05:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeliLine = [];
-function takeANumber(katzDeli, name) {
+function takeANumber(katzDeli, Ada) {
   katzDeli.push(`${Ada}`);
   return(`Welcome, ${Ada}. You are number ${katzDeli.length} in line.`);
 }
